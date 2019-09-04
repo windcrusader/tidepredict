a362b538506633545b2dc5bd7ad24eff26edc13f
windcrusader <ibeatspam@gmail.com>
2019-09-04 21:48:06 +1200

    0.0.1

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-conimport pathlib
+import pathlib
 import setuptools
 
 # The directory containing this file
@@ -9,21 +9,23 @@ README = (HERE / "README.md").read_text()
 
 # This call to setup() does all the work
 setuptools.setup(
-    name="psatools",
+    name="tidepredict",
     version="0.0.1",
-    description="Provides wrappers for electrical engineering calculations",
+    description="Tide Prediction tools",
     long_description=README,
     long_description_content_type="text/markdown",
-    url="https://github.com/windcrusader/psatools",
+    url="https://github.com/windcrusader/tidepredict",
     author="Brad Henderson",
     author_email="ba.henderson@gmail.com",
     license="MIT",
     classifiers=[
         "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7",
     ],
     packages=setuptools.find_packages(),
-    python_requires=">=3.6",
-    install_requires=["numpy"],
+    python_requires=">=2.7",
+    install_requires=["numpy","matplotlib","pandas","jinja2","scipy"],
 )
