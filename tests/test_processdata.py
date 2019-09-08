8ea1d6f3ceba7390a97057ea183b3569aa750c46
windcrusader <ibeatspam@gmail.com>
2019-09-08 21:39:39 +1200

    corrected prediction test to match time entry form

--- tests/test_processdata.py
+++ tests/test_processdata.py
@@ -17,21 +17,12 @@ def test_get_byte_stream():
     data = data.read()
 
 def test_prediction():
-    #todo need to make this generic for a particular day and minimal
-    #output.
-    args = __main__.parser.parse_args(["-l","Lyttelton"])
+    args = __main__.parser.parse_args(["-l","Lyttelton",
+                                        "-b", "2019-10-01 00:00",
+                                        "-e", "2019-10-01 08:00"])
     output = "\n".join(["All times in TZ: Pacific/Auckland",
-                       "2019-09-08 0558  0.59 Low Tide",
-                       "2019-09-08 1211  2.40 High Tide",
-                       "2019-09-08 1835  0.64 Low Tide",
-                        "2019-09-09 0034  2.26 High Tide",
-                        "2019-09-09 0651  0.63 Low Tide",
-                        "2019-09-09 1303  2.36 High Tide",
-                        "2019-09-09 1926  0.66 Low Tide",
-                        "2019-09-10 0126  2.22 High Tide",
-                        "2019-09-10 0741  0.66 Low Tide",
-                        "2019-09-10 1353  2.33 High Tide",
-                        "2019-09-10 2015  0.67 Low Tide"]) + "\n"
-    assert __main__.process_args(args) == output, "predictions don't match"
+                       "2019-10-01 0050  0.23 Low Tide",
+                       "2019-10-01 0702  2.64 High Tide"]) + "\n"
+    assert __main__.process_args(args) == output
 
 
